ef2ba29e70cf243b2fecaebbe443f44b0155e09a
Ali Baris Oztekin <alibarisoztekin@gmail.com>
2017-03-27 21:51:52 -0700

    add tableview prototypes

--- HomePact/UpcomingTVCell.swift
+++ HomePact/UpcomingTVCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class UpcomingTVCell: UITableViewCell {
 
+    @IBOutlet weak var taskImageContainerView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
