6cacc7dd6a0cb1ff3d962bfcc492e412ba7416f8
Ali Baris Oztekin <alibarisoztekin@gmail.com>
2017-03-27 20:20:43 -0700

    Add tabpageVc & storyboard, task VCs & storyboard

---
+++ HomePact/UpcomingTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  UpcomingTVCell.swift
+//  HomePact
+//
+//  Created by Ali Barış Öztekin on 2017-03-27.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
