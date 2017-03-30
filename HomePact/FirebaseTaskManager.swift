3bd2f5dc7e7a92b059bea1f2287cd181e766f719
Ali Baris Oztekin <alibarisoztekin@gmail.com>
2017-03-30 12:10:56 -0700

    setup firebase classes

---
+++ HomePact/FirebaseTaskManager.swift
@@ -0,0 +1,15 @@
+//
+//  FirebaseTaskManager.swift
+//  HomePact
+//
+//  Created by Ali Barış Öztekin on 2017-03-30.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+import UIKit
+
+class FirebaseTaskManager: NSObject {
+
+}
+
+
---
+++ HomePact/FirebaseUserManager.swift
@@ -0,0 +1,33 @@
+//
+//  FirebaseUserManager.swift
+//  HomePact
+//
+//  Created by Ali Barış Öztekin on 2017-03-30.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class FirebaseUserManager : NSObject {
+    
+    //MARK: Root References
+    
+    var usersRef: FIRDatabaseReference!
+    var userTaskLogsRef: FIRDatabaseReference!
+    var userMessageLogsRef: FIRDatabaseReference!
+    var userGroupLogsRef: FIRDatabaseReference!
+    
+//var userDetails =
+    
+    
+    
+//    init(parameters) {
+  //      <#statements#>
+  //  }
+    
+  //  func writeUserUpdates(in completion: )
+    
+    
+//}
+}
