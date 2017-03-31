d702b7b76f24ca4ab3e33d7e7c8517f2d13d8996
Ali Baris Oztekin <alibarisoztekin@gmail.com>
2017-03-30 19:41:14 -0700

    add Firebase Task User & Group managers

--- HomePact/FirebaseTaskManager.swift
+++ HomePact/FirebaseTaskManager.swift
@@ -7,8 +7,67 @@
 //
 
 import UIKit
+import Firebase
+
+
+enum TaskCondition:String {
+    case upcoming = "upcoming", completed = "completed"
+}
+
 
 class FirebaseTaskManager: NSObject {
+
+    //MARK: Root References
+    var rootRef: FIRDatabaseReference
+    var tasksRef: FIRDatabaseReference
+    var taskLogsRef: FIRDatabaseReference
+    
+    
+    
+    override init() {
+        
+        self.rootRef = FIRDatabase.database().reference()
+        self.tasksRef = rootRef.child("tasks")
+        self.taskLogsRef = rootRef.child("taskLogs")
+        
+    }
+
+   
+    func getTasks(with IDs:[String], with closure:@escaping (_ tasks:[Task],_ error:Error?)-> (Void) ){
+        
+        
+        let query = tasksRef.queryOrderedByKey().queryStarting(atValue: IDs.first).queryEnding(atValue: IDs.last)
+        query.observe(.value, with: {snapshot in
+        
+            let result = self.tasks(from: snapshot, with: IDs)
+            closure(result.tasks, result.error)
+        })
+        
+    
+    
+
+        
+    }
+    
+    func tasks(from snapshot:FIRDataSnapshot, with IDs:[String]) ->(tasks:[Task], error:Error?){
+        
+        var tasks = [Task]()
+        
+        guard let value = snapshot.value as? NSDictionary else {
+            let closureError = "Error accesing groups  IDs" as! Error
+            return ([],closureError)
+        }
+        
+        let filteredTasks = value.filter({ (key, value) in
+            return IDs.contains(key as! String)
+        })
+        
+        //filteredTasks to Task struct parsing
+        
+
+        
+        return (tasks,nil)
+    }
 
 }
 
