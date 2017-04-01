1ab82a4997fb2c36db1236c58e6849f28e495468
callosaurus <callumdavies421@gmail.com>
2017-04-01 16:11:11 -0700

    Add NotificationManager

---
+++ HomePact/NotificationManager.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationManager.swift
+//  HomePact
+//
+//  Created by Callum Davies on 2017-04-01.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+import UIKit
+
+class NotificationManager: NSObject {
+
+}
