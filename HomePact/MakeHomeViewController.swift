18ba1dbf13bb5d84f92ec254b09370f223f643db
daveaugerinos <dave.augerinos@gmail.com>
2017-04-04 12:41:05 -0700

    Add search for existing group name

--- HomePact/MakeHomeViewController.swift
+++ HomePact/MakeHomeViewController.swift
@@ -92,62 +92,33 @@ class MakeHomeViewController: UIViewController, UIImagePickerControllerDelegate,
             alert(title: "Invalid Home Name", message: "Your home name must have minimum of 5 alphabet characters and no spaces.")
         }
         
-        // TESTING
-        let key = FirebaseGroupManager().groupsRef.childByAutoId().key
-        var group = Group(id: key, name: homeName, timestamp: Date())
-        group.groupImage = currentImage
-        FirebaseGroupManager().update(group)
-        print(group)
-        
-        var ref: FIRDatabaseReference!
-        
-        ref = FIRDatabase.database().reference()
-        let groupRef = ref.child("groups")
-        
-//        groupRef.observe(.value, with: { (snapshot) -> Void in
-//            print("SNAPSHOT: \(snapshot)")
-//        })
-        
-        groupRef.queryOrdered(byChild:"name").queryEqual(toValue:homeName).observe(.value, with: { snapshot in
-            // Returns all groups with state "open"
-            for group in snapshot.children {
-                print("SEARCH FOR \(homeName) already exist -> \(group)")
-            }
-        })
-        
-        
-//        groupRef.observeSingleEvent(of: .childAdded, with: { (snapshot: FIRDataSnapshot) in
-//            if let dict = snapshot.value as? [String:AnyObject]{
-//                for item in dict {
-//                    print(item)
-//                    print(item.key)
-//                    print(item.value)
-//                }
-//            }
-//        })
-        
-        // Check if home already exists
-//        FirebaseGroupManager().checkExisting(groupName: homeName, closure: { name in
-//            if name == false {
-//                self.alert(title: "Invalid Home Name", message: "This home name is already in use.")
-//            }
-//            
-//            // If home name does not exist, then add it to database
-//            else {
-//                // Provide feedback while making network call
-//                self.activityIndicator.startAnimating()
-//                
-//                let key = FirebaseGroupManager().groupsRef.childByAutoId().key
-//                var group = Group(id: key, name: homeName, timestamp: Date())
-//                group.groupImage = currentImage
-//                FirebaseGroupManager().update(group)
-//                
-//                if(FirebaseGroupManager().addCurrentUser(group: group)) {
-//                    self.activityIndicator.stopAnimating()
-//                    self.createHomeFeedbackView.layer.isHidden = false
-//                }
-//            }
-//        })
+        else {
+            activityIndicator.startAnimating()
+            
+            var ref: FIRDatabaseReference!
+            ref = FIRDatabase.database().reference()
+            let groupRef = ref.child("groups")
+            
+            groupRef.queryOrdered(byChild:"name").queryEqual(toValue:homeName).observe(.value, with: { snapshot in
+                
+                if (snapshot.children.allObjects.count > 0) {
+                    self.alert(title: "Invalid Home Name", message: "This name is already in use.  Please choose another one.")
+                    self.activityIndicator.stopAnimating()
+                    self.createHomeFeedbackView.layer.isHidden = true // REMOVE THIS AFTER TESTING
+                }
+                    
+                else {
+                    let key = FirebaseGroupManager().groupsRef.childByAutoId().key
+                    var group = Group(id: key, name: homeName, timestamp: Date())
+                    group.groupImage = currentImage
+                    FirebaseGroupManager().update(group)
+                    self.activityIndicator.stopAnimating()
+                    self.createHomeFeedbackView.layer.isHidden = false
+                }
+                
+                self.activityIndicator.stopAnimating()
+            })
+        }
     }
     
     @IBAction func sendInviteButtonTouched(_ sender: UIButton) {
