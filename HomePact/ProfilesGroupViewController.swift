1c703c197d1a488a378945e2da2f526b9294906d
callosaurus <callumdavies421@gmail.com>
2017-04-05 12:33:41 -0700

    Update Profiles group/personal view controllers

--- HomePact/ProfilesGroupViewController.swift
+++ HomePact/ProfilesGroupViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ProfilesGroupViewController: UIViewController {
+class ProfilesGroupViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet fileprivate weak var groupImageView: UIImageView!
     @IBOutlet fileprivate weak var groupNameLabel: UILabel!
     @IBOutlet fileprivate weak var groupUsersTableView: UITableView!
     @IBOutlet fileprivate weak var leaveButton: UIButton!
+    var arrayOfUsers : [User] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,10 +22,20 @@ class ProfilesGroupViewController: UIViewController {
         roundAppropriateViews()
         self.view.backgroundColor = UIColor.lightGray
         
-        let fbUM = FirebaseUserManager()
-        let fbGM = FirebaseGroupManager()
         
-
+        FirebaseGroupManager().currentUserGroup { (group, error) -> (Void) in
+            self.groupImageView.image = group?.groupImage
+            self.groupNameLabel.text = group?.name
+            
+            FirebaseGroupManager().observeUserIDs(for: group!) { (arrayOfIDs, error) -> (Void) in
+                let arrayOfUserIDs = arrayOfIDs
+                
+                FirebaseUserManager().usersWith(userIDs: arrayOfUserIDs) { (returnedUsers, error) -> (Void) in
+                    self.arrayOfUsers = returnedUsers!
+                    self.groupUsersTableView.reloadData()
+                }
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -33,7 +44,20 @@ class ProfilesGroupViewController: UIViewController {
     }
     
     @IBAction func leaveGroupButtonPressed(_ sender: UIButton) {
-        //alert controller to ask for confirmation
+        let alert = UIAlertController(title: "Leave Group", message: "Are you sure?", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "I'm sure", style: .default) { action in
+            FirebaseUserManager().currentUser { (user) in
+                FirebaseGroupManager().currentUserGroup { (group, error) -> (Void) in
+                    FirebaseGroupManager().remove(user: user!, from: group!)
+                }
+            }
+            
+            self.dismiss(animated: true, completion: nil)
+        })
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default) { action in
+            self.dismiss(animated: true, completion: nil)
+        })
+        self.present(alert, animated: true)
     }
 
     func roundAppropriateViews() {
@@ -42,9 +66,21 @@ class ProfilesGroupViewController: UIViewController {
         self.groupImageView.layer.cornerRadius = groupImageView.frame.width/2
         
         self.groupNameLabel.layer.cornerRadius = 3.0
-        self.leaveButton.layer.cornerRadius = 3.0
     }
     
+    //MARK: TableView Methods
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as! GroupTableViewCell
+        let userToDisplay = arrayOfUsers[indexPath.row]
+        cell.user = userToDisplay
+        return cell
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfUsers.count
+    }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 }
